ee93a937edcb53ebcd306d5117a400fe1d36bfbe
bywilfried <bywilfried@hotmail.com>
2026-09-25 23:00:56 +0200

    Use focused Prime ordering menu

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/PrimeDrawerFolderAppsPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/PrimeDrawerFolderAppsPreference.kt
@@ -50,6 +50,7 @@ fun PrimeDrawerFolderAppsPreference(tabId: String, folderId: String) {
             if (folder.sortMode == "custom") repository.setFolderCustomOrder(tabId, folderId, componentKeys)
         },
         showDuplicateFilter = false,
+        showStandardMenuActions = false,
         preserveActiveOrder = folder.sortMode == "custom",
         extraMenuContent = { hideMenu ->
             DropdownMenuItem(
