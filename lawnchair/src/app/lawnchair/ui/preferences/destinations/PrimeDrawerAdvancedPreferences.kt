9fc5364a703054286acdaae95dad5ca2449c49c5
bywilfried <bywilfried@hotmail.com>
2026-09-26 19:08:30 +0200

    Fix Prime color control layout import

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/PrimeDrawerAdvancedPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/PrimeDrawerAdvancedPreferences.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
